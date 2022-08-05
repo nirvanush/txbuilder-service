0e9bbe93e44234e72d197526da96411646cf6c82
nirvanush <nirvanushergo@gmail.com>
2022-08-05 17:27:34 +0300

    release

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lite weight javascript service to build transaction
+
+Can run on aws serverless lambda or on a server with `npm run dev`;
+I suggest trying lambda because it's free.
+
+### Based on: 
+- [ergoscript.js](https://github.com/nirvanush/ergoscript) - transaction builder library
+- [isomorphic-wallet] - in code wallet with same api as dapp connector.
+
+
+## How to start
+```
+git clone https://github.com/nirvanush/txbuilder-service
+cd txbuilder-service
+npm i 
+npm run start
+```
+
+### TODO:
+- Custom explorer endpoint, currently using public.
--- handler.js
+++ handler.js
@@ -2,40 +2,35 @@
 const ErgoWallet = require('isomorphic-wallet').default;
 const Transaction = require('ergoscript').default;
 
-module.exports.handler = async (event) => {
+module.exports.txBuilder = async (event) => {
   const apiToken = event.headers['x-api-key'];
 
-  if (!apiToken || apiToken !== process.env.INTERNAL_API_TOKEN) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ message: 'Rejected' }),
-    };
-  }
+  // if (!apiToken || apiToken !== process.env.INTERNAL_API_TOKEN) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify({ message: 'Rejected' }),
+  //   };
+  // }
 
-  const { config } = JSON.parse(event.body);
-  console.log('received-body', config);
-  const mintWallet = await new ErgoWallet().fromMnemonics(process.env.SEED || '');
+  const { config, userAddress } = JSON.parse(event.body);
 
-  mintWallet.setPublicAddress(process.env.PK);
+  const mintWallet = await new ErgoWallet().fromMnemonics('');
+
+  mintWallet.setPublicAddress(userAddress);
 
   const unsignedTx = await (new Transaction(config, { wallet: mintWallet })).build();
 
-
   try {
-    const signedTx = await mintWallet.sign_tx(unsignedTx.toJSON());
-    const hash = await mintWallet.submit_tx(signedTx);
-
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'OK', hash }),
+      body: JSON.stringify({ message: 'OK', unsignedTx: unsignedTx.toJSON() }),
     };
 
   } catch(e) {
     console.error(e);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Error' }),
+      body: JSON.stringify({ message: e.message }),
     };
   }
-
 };
